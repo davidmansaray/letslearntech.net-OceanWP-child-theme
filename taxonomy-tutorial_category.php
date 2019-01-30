99cd0b0179b094d679f9d848ab185b5523bdf14b
David Mansaray <david@davidmansaray.com>
2019-01-30 17:47:19 -0300

    no message

--- taxonomy-tutorial_category.php
+++ taxonomy-tutorial_category.php
@@ -9,20 +9,24 @@
  */
 
 get_header(); ?>
+    
 
 	<?php do_action( 'ocean_before_content_wrap' ); ?>
 
 	<div id="content-wrap" class="container clr">
 
 		<?php do_action( 'ocean_before_primary' ); ?>
+		
 
 		<div id="primary" class="content-area clr">
 
 			<?php do_action( 'ocean_before_content' ); ?>
-
+			
+            
 			<div id="content" class="site-content clr">
 
 				<?php do_action( 'ocean_before_content_inner' ); ?>
+				
 
 				<?php
 				// Elementor `single` location
@@ -50,11 +54,24 @@ get_header(); ?>
 		    					get_template_part( 'partials/library/layout' );
 
 		    				}
+					
+								// Tutorial post types
+		    				elseif ( is_singular( 'oceanwp_library' )
+		    						|| is_singular( 'elementor_library' ) ) {
+
+		    					get_template_part( 'partials/tutorial/layout' );
+								
+								
+
+		    				}
+
 
 							// All other post types.
 							else {
 
-		    					get_template_part( 'partials/single/layout', get_post_type() );
+		    					get_template_part( 'partials/tutorial/layout', get_post_type() );
+                                
+                                
 
 		  					}
 
@@ -63,6 +80,7 @@ get_header(); ?>
 				} ?>
 
 				<?php do_action( 'ocean_after_content_inner' ); ?>
+				
 
 			</div><!-- #content -->
 
